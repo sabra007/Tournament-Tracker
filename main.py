0e74e225ba475282ee22733fbd4d7be52a9d0519
Sargis <sargis.ab@gmail.com>
2021-12-22 14:40:44 -0600

    add view participans and cancel signup

--- main.py
+++ main.py
@@ -17,35 +17,88 @@ def setUp():
             print("Invalid Input. Try again.") 
 
     number_of_participants = int(num)
-    patricipants_dict = {}
+    participants_dict = {}
 
     for i in range(0, number_of_participants):
-        patricipants_dict[i] = None
+        participants_dict[i] = None
 
+    return participants_dict
+
+def exit_app(participants_dict):
+    exit()
+
+def cancelSignUp(participants_dict):
+    print("  Participant Cancellation ")
+    print("===========================")
     
-    return patricipants_dict
+    while 1:
 
-def exit_app():
-    pass
+        if input("Enter 0 to go back to main menu\nEnter anything else to continue: ") == '0':
+            break;
 
-def cancelSignUp():
-    pass
+        desired_slot = slotSelectionValidation(participants_dict, "Starting slot")
 
-def viewParticipants():
-    pass
+        name = input("Participan Name: ")
+        
+        if participants_dict[desired_slot] == name:
+            participants_dict[desired_slot] = None
+            print(f"Success:\n{name} has been cancelled from starting slot #{desired_slot}.")
+            break;
+        else:
+            print(f"{name} is not in that starting slot.")
+
+
+def viewParticipants(participants_dict):
+    print("     View Participants     ")
+    print("===========================")
+
+    desired_slot = slotSelectionValidation(participants_dict, "Starting slot")
+
+    slots_range_max = min(desired_slot + 6, len(participants_dict))
+    slots_range_min = max(desired_slot - 5, 1)
+
+    print("\nStarting Slot: Participant")
+    for i in range(slots_range_min, slots_range_max):
+        if participants_dict[i] == None:
+            print(f"{i}: [empty]")
+        else:
+            print(f"{i}: {participants_dict[i]}")
+
+
 
 def saveChanges():
     pass
 
 
-def slotSelectionValidation(participant_dict):
-    desired_slot = input(f"Desired starting slot #[1-{len(participant_dict)}]: ")
+def slotSelectionValidation(participant_dict, message):
+    desired_slot = input(f"{message} #[1-{len(participant_dict)}]: ")
     while 1:
         if desired_slot.isdigit() and int(desired_slot) > 0 and int(desired_slot) <= len(participant_dict):
             return int(desired_slot)
         else:
             print("Invalid input. Try again.")
 
+
+
+# prompt for participant name and desired slot
+# check if the slot is available
+def signUp(participants_dict):
+    print("    Participant Sign Up    ")
+    print("===========================")
+    name = input("Participant Name: ")
+
+    while 1:
+
+        desired_slot = slotSelectionValidation(participants_dict, "Desired starting slot")
+
+
+        if participants_dict[desired_slot] == None:
+            participants_dict[desired_slot] = name
+            print(f"Success:\n{name} is signed up in the starting slot #{desired_slot}: ")
+            break;
+        else:
+            print(f"Error:\nSlot #{desired_slot} is filled. Please try again")
+    
 
 # Menu
 def mainMenu(participant_dict):
@@ -69,7 +122,6 @@ def mainMenu(participant_dict):
             else:
                 print("Invalid Input. Try again.")
 
-        print(f"User Choice: {choice}")
         menu = [signUp, cancelSignUp, viewParticipants, saveChanges, exit_app]
 
         menu[choice-1](participant_dict)
@@ -77,25 +129,7 @@ def mainMenu(participant_dict):
     
 
 
-# prompt for participant name and desired slot
-# check if the slot is available
-def signUp(participants_dict):
-    print("    Participant Sign Up    ")
-    print("===========================")
-    name = input("Participant Name: ")
 
-    while 1:
-
-        desired_slot = slotSelectionValidation(participants_dict)
-
-
-        if participants_dict[desired_slot] == None:
-            participants_dict[desired_slot] = name
-            print(f"Success:\n{name} is signed up in the starting slot #{desired_slot}: ")
-            break;
-        else:
-            print(f"Error:\nSlot #{desired_slot} is filled. Please try again")
-    
 
 def startUp():
     print(" Welcome to the Tournament ")
